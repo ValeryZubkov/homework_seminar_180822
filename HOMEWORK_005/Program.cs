cfe0d6b3ea4e9eb1896667379d3e7018a081cba8
Valery Zubkov <bouddha@mail.ru>
2022-08-24 16:21:41 +0400

    homework number 5

---
+++ HOMEWORK_005/Program.cs
@@ -0,0 +1,20 @@
+﻿//Солдат и бананы
+Console.WriteLine("Задайте стоимость первого банана k: ");
+int k = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Задайте количество денег у солдата n: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Задайте желаемое количество бананов w: ");
+int w = int.Parse(Console.ReadLine() ?? "0");
+int sum = 0;
+for(int i = 1; i <= w; i++)
+{
+  sum = sum + k * i;
+}
+if(sum > n)
+{
+    Console.WriteLine($"Солдату не хватает {sum - n} долларов.");
+}
+else
+{
+    Console.WriteLine($"У солдата достаточно денег для покупки {w} бананов.");
+}
